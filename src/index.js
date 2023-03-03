ceb8f940ddbfda6b87154355d5629d4bf695398d
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-03 06:57:18 +0044

    Requisito 1 concluído

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const getTalkers = async () => {
+  const data = await fs.promises.readFile(path.resolve(__dirname, './talker.json'));
+  const json = JSON.parse(data);
+  return json;
+};
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await getTalkers(); 
+
+  if (talkers) {
+    res.status(HTTP_OK_STATUS).json(talkers);
+  } else {
+    res.status(HTTP_OK_STATUS).json([]);
+  }
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
