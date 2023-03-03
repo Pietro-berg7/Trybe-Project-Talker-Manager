dc008ffa0e097fcd2433c852de56a9bf78881136
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-03 07:37:41 +0044

    Requisito 2 concluído

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND = 404;
 const PORT = '3000';
 
 const getTalkers = async () => {
@@ -14,6 +15,12 @@ const getTalkers = async () => {
   return json;
 };
 
+const getById = async (id) => {
+  const data = await getTalkers();
+  return data.find((e) => e.id === id);
+};
+
+// endpoint GET /talker
 app.get('/talker', async (_req, res) => {
   const talkers = await getTalkers(); 
 
@@ -21,6 +28,20 @@ app.get('/talker', async (_req, res) => {
     res.status(HTTP_OK_STATUS).json(talkers);
   } else {
     res.status(HTTP_OK_STATUS).json([]);
+  }
+});
+
+// endpoint GET /talker/:id
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await getById(Number(id));
+  
+  const message = 'Pessoa palestrante não encontrada';
+
+  if (talker) {
+    res.status(HTTP_OK_STATUS).json(talker);
+  } else {
+    res.status(HTTP_NOT_FOUND).json({ message });
   }
 });
 
